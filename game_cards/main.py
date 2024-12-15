986700b01fcc6548042674bfcbb9e7714d0d5dc1
ilaymar95 <108624999+ilaymar95@users.noreply.github.com>
2024-12-15 15:53:28 +0200

    Updated test_card_game

--- game_cards/main.py
+++ game_cards/main.py
@@ -1,6 +1,9 @@
+from time import sleep
+
 from card_game import CardGame
 
 print("Card game will begin shorty!")
+sleep(1)
 cards_to_deal=int(input("How many cards do you want? 10-26: "))
 name1 = input("What is your name player 1?: ")
 name2 = input("What is your name player 2?: ")
@@ -9,13 +12,16 @@ print(card_game.player1)
 print(card_game.player2)
 count_player1=0
 count_player2=0
-#Testing new_game()
-# card_game.new_game()
+
 for i in range (10):
     print(f"Round {i+1} started\n")
+    sleep(1)
     player1_card = card_game.player1.get_card()
+    print(f'{card_game.player1.name} took out {player1_card}')
+    sleep(1)
     player2_card = card_game.player2.get_card()
-    print(f'{card_game.player1.name} took out {player1_card}\n{card_game.player2.name} took out {player2_card}')
+    print(f'{card_game.player2.name} took out {player2_card}')
+    sleep(1)
     if player1_card > player2_card:
         print(f'{card_game.player1.name} won the round!')
         count_player1+=1
@@ -26,7 +32,9 @@ for i in range (10):
         count_player2+=1
         card_game.player2.add_card(player1_card)
         card_game.player2.add_card(player2_card)
+    print()
     print(f"Round {i+1} finished\n")
+    sleep(1)
 
 if card_game.get_winner() is None:
     print('The game ended in a tie!')
